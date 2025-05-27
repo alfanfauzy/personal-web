dd19374afa0335697674aecc3d91c5ac40eb0ae8
alfanfauzy <alfan.fauzy@moduit.id>
2025-05-27 14:42:52 +0700

    feat(style): add style for slider animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -71,10 +71,20 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "slide-left": {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-50%)" },
+        },
+        "slide-right": {
+          "0%": { transform: "translateX(-50%)" },
+          "100%": { transform: "translateX(0)" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "slide-left": "slide-left 30s linear infinite",
+        "slide-right": "slide-right 30s linear infinite",
       },
     },
   },
