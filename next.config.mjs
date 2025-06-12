1e38717aaaf71812bcdcbbe42041e9403467e262
alfanfauzy <zhafhy@gmail.com>
2025-06-12 22:47:21 +0700

    feat(PWA): add PWA in personal website

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
+import nextPwa from "next-pwa";
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
 };
 
-export default nextConfig;
+const withPWA = nextPwa({
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+});
+
+export default withPWA(nextConfig);
