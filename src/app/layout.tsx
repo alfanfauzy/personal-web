790baaf516ac008586b0c38ab5b8666dcae5609d
alfanfauzy <zhafhy@gmail.com>
2025-09-29 15:39:51 +0700

    fix(blog): fix formater date

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,69 +6,72 @@ import InnerLayout from "./blog/inner-layout";
 import RegisterSW from "@/components/register-sw";
 
 const fontSans = FontSans({
-    subsets: ["latin"],
-    variable: "--font-sans",
+  subsets: ["latin"],
+  variable: "--font-sans",
 });
 
 export const metadata: Metadata = {
-    metadataBase: new URL(DATA.url),
-    title: {
-        default: DATA.name,
-        template: `%s | ${DATA.name}`,
-    },
+  metadataBase: new URL(DATA.url),
+  title: {
+    default: DATA.name,
+    template: `%s | ${DATA.name}`,
+  },
+  description: DATA.description,
+  openGraph: {
+    title: `${DATA.name}`,
     description: DATA.description,
-    openGraph: {
-        title: `${DATA.name}`,
-        description: DATA.description,
-        url: DATA.url,
-        siteName: `${DATA.name}`,
-        locale: "en_US",
-        type: "website",
+    url: DATA.url,
+    siteName: `${DATA.name}`,
+    locale: "en_US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
     },
-    robots: {
-        index: true,
-        follow: true,
-        googleBot: {
-            index: true,
-            follow: true,
-            "max-video-preview": -1,
-            "max-image-preview": "large",
-            "max-snippet": -1,
-        },
-    },
-    twitter: {
-        title: `${DATA.name}`,
-        card: "summary_large_image",
-    },
-    verification: {
-        google: "",
-        yandex: "",
-    },
-    icons: {
-        icon: "/fav-me-icon.ico",
-        apple: "/icons/icon-192x192.png",
-    },
-    manifest: "/manifest.json", // ✅ Tambahkan ini
-    themeColor: "#0f172a", // ✅ Sesuaikan dengan warna tema kamu
+  },
+  twitter: {
+    title: `${DATA.name}`,
+    card: "summary_large_image",
+  },
+  verification: {
+    google: "",
+    yandex: "",
+  },
+  icons: {
+    icon: "/fav-me-icon.ico",
+    apple: "/icons/icon-192x192.png",
+  },
+  manifest: "/manifest.json", // ✅ Tambahkan ini
+  themeColor: "#0f172a", // ✅ Sesuaikan dengan warna tema kamu
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en" suppressHydrationWarning>
-            <head>
-                <link rel="manifest" href="/manifest.json" />
-                <link rel="icon" href="/fav-me-icon.ico" sizes="any" />
-                <meta name="theme-color" content="#0f172a" />
-                <script defer src="https://cloud.umami.is/script.js" data-website-id="ae52596e-a43c-48b0-92fd-05b1a793f84e"></script>
-            </head>
-            <body>
-                <RegisterSW />
-                <InnerLayout>{children}</InnerLayout>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/fav-me-icon.ico" sizes="any" />
+        <meta name="theme-color" content="#0f172a" />
+        <script
+          defer
+          src="https://cloud.umami.is/script.js"
+          data-website-id="ae52596e-a43c-48b0-92fd-05b1a793f84e"
+        ></script>
+      </head>
+      <body>
+        <InnerLayout>{children}</InnerLayout>
+      </body>
+    </html>
+  );
 }
