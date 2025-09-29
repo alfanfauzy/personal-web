ee18edcd0e9b67528b8e3c9f01b3195bbf8e9558
alfanfauzy <zhafhy@gmail.com>
2025-09-29 15:40:31 +0700

    fix(blog): loging data blog in server

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,24 +3,25 @@ import BlogCard from "@/components/blog-card";
 import Title from "@/components/ui/title";
 
 const BlogPage = async () => {
-    const data = await getAllBlogs();
+  const data = await getAllBlogs();
 
-    return (
-        <section>
-            <Title title={"My Blogs"} />
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-                {data
-                    .sort((a, b) =>
-                        new Date(a.published_at) > new Date(b.published_at)
-                            ? -1
-                            : 1
-                    )
-                    .map((post, id) => (
-                        <BlogCard post={post} key={id} />
-                    ))}
-            </div>
-        </section>
-    );
+  console.log("print data length at server: ", data.length);
+  console.log("print data at server: ", data);
+
+  return (
+    <section>
+      <Title title={"My Blogs"} />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        {data
+          .sort((a, b) =>
+            new Date(a.published_at) > new Date(b.published_at) ? -1 : 1
+          )
+          .map((post, id) => (
+            <BlogCard post={post} key={id} />
+          ))}
+      </div>
+    </section>
+  );
 };
 
 export default BlogPage;
